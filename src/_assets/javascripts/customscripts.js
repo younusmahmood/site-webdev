f5776a64c137948368ec34e81ed42165f3acb50f
Eric Jimenez <eric.jimenez.dev@gmail.com>
2016-11-21 17:15:50 -0500

    WIP

---
+++ src/_assets/javascripts/customscripts.js
@@ -0,0 +1,53 @@
+$( document ).ready(function() {
+
+    // this script says, if the height of the viewport is greater than 800px,
+    // then insert affix class, which makes the nav bar float in a fixed
+    // position as your scroll. if you have a lot of nav items, this height may
+    // not work for you.
+    var h = $(window).height();
+    //console.log (h);
+    if (h > 800) {
+        $( "#mysidebar" ).attr("class", "nav affix");
+    }
+    // activate tooltips. although this is a bootstrap js function, it must be activated this way in your theme.
+    $('[data-toggle="tooltip"]').tooltip({
+        placement : 'top'
+    });
+});
+
+$(document).ready(function() {
+    // Initialize navgoco with default options
+    $("#mysidebar").navgoco({
+        caretHtml: '',
+        accordion: true,
+        openClass: 'active', // open
+        save: false, // leave false or nav highlighting doesn't work right
+        cookie: {
+            name: 'navgoco',
+            expires: false,
+            path: '/'
+        },
+        slide: {
+            duration: 400,
+            easing: 'swing'
+        }
+    });
+
+    $("#collapseAll").click(function(e) {
+        e.preventDefault();
+        $("#mysidebar").navgoco('toggle', false);
+    });
+
+    $("#expandAll").click(function(e) {
+        e.preventDefault();
+        $("#mysidebar").navgoco('toggle', true);
+    });
+
+    $("#mysidebar li.active").parents('li').toggleClass("active");
+    $("#mysidebar li.active").parents('ul').show();
+
+    /**
+     * AnchorJS
+     */
+    anchors.add('h2,h3,h4,h5');
+});
--- src/_assets/javascripts/main.js
+++ src/_assets/javascripts/main.js
@@ -1,4 +1,5 @@
 //= require vendor/jquery-3.1.1.min
+//= require vendor/jquery.navgoco.min
 //= require bootstrap
 //= require _utilities
 //= require _search
@@ -129,5 +130,54 @@ $(document).ready(function(){
   // Add external link indicators
   $('a[href^="http"], a[target="_blank"]').not('.no-automatic-external').addClass('external');
 
+});
+
+$( document ).ready(function() {
+
+    // this script says, if the height of the viewport is greater than 800px,
+    // then insert affix class, which makes the nav bar float in a fixed
+    // position as your scroll. if you have a lot of nav items, this height may
+    // not work for you.
+    var h = $(window).height();
+    //console.log (h);
+    if (h > 800) {
+        $( "#mysidebar" ).attr("class", "nav affix");
+    }
+    // activate tooltips. although this is a bootstrap js function, it must be activated this way in your theme.
+    $('[data-toggle="tooltip"]').tooltip({
+        placement : 'top'
+    });
+});
+
+$(document).ready(function() {
+    // Initialize navgoco with default options
+    $("#mysidebar").navgoco({
+        caretHtml: '',
+        accordion: true,
+        openClass: 'active', // open
+        save: false, // leave false or nav highlighting doesn't work right
+        cookie: {
+            name: 'navgoco',
+            expires: false,
+            path: '/'
+        },
+        slide: {
+            duration: 400,
+            easing: 'swing'
+        }
+    });
+
+    $("#collapseAll").click(function(e) {
+        e.preventDefault();
+        $("#mysidebar").navgoco('toggle', false);
+    });
+
+    $("#expandAll").click(function(e) {
+        e.preventDefault();
+        $("#mysidebar").navgoco('toggle', true);
+    });
+
+    $("#mysidebar li.active").parents('li').toggleClass("active");
+    $("#mysidebar li.active").parents('ul').show();
 });
 
